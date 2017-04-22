6a53aa78edb0788dba73b148c7f0fb35e69103b9
Gernot Raudner <gitkraken@gernotraudner.at>
2017-04-22 16:59:39 +0200

    sprites for entities
    also added sprinter, heavy and archer

--- Assets/scripts/entities/Entity.cs
+++ Assets/scripts/entities/Entity.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace scripts.entities {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class Entity : MonoBehaviour {
         [SerializeField]
         private EntityType entityType;
+
+        private SpriteRenderer spriteRenderer;
+
         private float maxHealth;
         private float range;
         private float movementSpeed;
@@ -14,6 +18,9 @@ namespace scripts.entities {
         private Sprite sprite;
         // Use this for initialization
         void Start() {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = entityType.EntitySprite;
+
             maxHealth = entityType.MaxHealth;
             range = entityType.Range;
             movementSpeed = entityType.MovementSpeed;
@@ -24,7 +31,8 @@ namespace scripts.entities {
 
         // Update is called once per frame
         void Update() {
-
+            //TODO: remove after testing
+            transform.position = transform.position + movementSpeed * Time.deltaTime * Vector3.right;
         }
     }
 }
