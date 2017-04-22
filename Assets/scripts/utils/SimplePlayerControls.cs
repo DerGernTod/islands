154ed9f45c62cc4755993e08995004f7c941196c
Unknown <evabrucker@aon.at>
2017-04-22 19:26:53 +0200

    Simple Network Player Movement

---
+++ Assets/scripts/utils/SimplePlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerControls : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        x *= Time.deltaTime;
+        y *= Time.deltaTime;
+
+        transform.Translate(x, y, 0);
+	}
+}
