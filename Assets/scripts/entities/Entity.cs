de917fbf2a0c5d35c8fd7bce5adb983f18c35cf2
Gernot Raudner <gitkraken@gernotraudner.at>
2017-04-22 16:30:22 +0200

    added entity and entitytype

---
+++ Assets/scripts/entities/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scripts.entities {
+    public class Entity : MonoBehaviour {
+        [SerializeField]
+        private EntityType entityType;
+        private float maxHealth;
+        private float range;
+        private float movementSpeed;
+        private float attackSpeed;
+        private float damage;
+        private Sprite sprite;
+        // Use this for initialization
+        void Start() {
+            maxHealth = entityType.MaxHealth;
+            range = entityType.Range;
+            movementSpeed = entityType.MovementSpeed;
+            attackSpeed = entityType.AttackSpeed;
+            damage = entityType.Damage;
+            sprite = entityType.EntitySprite;
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Assets/scripts/entities/EntityType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scripts.entities {
+    [CreateAssetMenu(fileName = "Data", menuName = "Entity/EntityType", order = 1)]
+    public class EntityType : ScriptableObject {
+
+        public float MaxHealth = 1;
+        public float Range = 1;
+        public float MovementSpeed = 1;
+        public float AttackSpeed = 1;
+        public float Damage = 1;
+        public Sprite EntitySprite;
+    }
+}
