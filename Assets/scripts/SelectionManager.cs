2422c04820342d351b2795c7dd0a88748dad8e35
Benji1 <Benji1@users.noreply.github.com>
2017-04-23 13:35:46 +0200

    selecting and moving units

---
+++ Assets/scripts/SelectionManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour {
+    bool isSelecting = false;
+    Vector3 mousePosition1;
+
+    public GameObject selectionCirclePrefab;
+
+    void Update() {
+        // If we press the left mouse button, begin selection and remember the location of the mouse
+        if (Input.GetMouseButtonDown(0)) {
+            isSelecting = true;
+            mousePosition1 = Input.mousePosition;
+
+            foreach (var selectableObject in FindObjectsOfType<SelectableUnit>()) {
+                if (selectableObject.selectionCircle != null) {
+                    Destroy(selectableObject.selectionCircle.gameObject);
+                    selectableObject.selectionCircle = null;
+                    if (selectableObject.GetComponent<ClickToMove>() != null)
+                    {
+                        selectableObject.GetComponent<ClickToMove>().enabled = false;
+                    }
+                }
+            }
+        }
+        // If we let go of the left mouse button, end selection
+        if (Input.GetMouseButtonUp(0)) {
+            var selectedObjects = new List<SelectableUnit>();
+            foreach (var selectableObject in FindObjectsOfType<SelectableUnit>()) {
+                if (IsWithinSelectionBounds(selectableObject.gameObject)) {
+                    selectedObjects.Add(selectableObject);
+                }
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("Selecting [{0}] Units", selectedObjects.Count));
+            foreach (var selectedObject in selectedObjects)
+                sb.AppendLine("-> " + selectedObject.gameObject.name);
+            Debug.Log(sb.ToString());
+
+            isSelecting = false;
+        }
+
+        // Highlight all objects within the selection box
+        if (isSelecting) {
+            foreach (var selectableObject in FindObjectsOfType<SelectableUnit>()) {
+                if (IsWithinSelectionBounds(selectableObject.gameObject)) {
+                    if (selectableObject.selectionCircle == null) {
+                        selectableObject.selectionCircle = Instantiate(selectionCirclePrefab);
+                        selectableObject.selectionCircle.transform.SetParent(selectableObject.transform, false);
+                        selectableObject.selectionCircle.transform.eulerAngles = new Vector3(0, 0, 0);
+
+                        if (selectableObject.GetComponent<ClickToMove>() != null)
+                        {
+                            selectableObject.GetComponent<ClickToMove>().enabled = true;
+                        }
+                    }
+                }
+                else {
+                    if (selectableObject.selectionCircle != null) {
+                        Destroy(selectableObject.selectionCircle.gameObject);
+                        selectableObject.selectionCircle = null;
+                    }
+                }
+            }
+        }
+    }
+
+    public bool IsWithinSelectionBounds(GameObject gameObject) {
+        if (!isSelecting)
+            return false;
+
+        var camera = Camera.main;
+        var viewportBounds = UnitSelectionUtils.GetViewportBounds(camera, mousePosition1, Input.mousePosition);
+        return viewportBounds.Contains(camera.WorldToViewportPoint(gameObject.transform.position));
+    }
+
+    void OnGUI() {
+        if (isSelecting) {
+            // Create a rect from both mouse positions
+            var rect = UnitSelectionUtils.GetScreenRect(mousePosition1, Input.mousePosition);
+            UnitSelectionUtils.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+            UnitSelectionUtils.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
+        }
+    }
+}
